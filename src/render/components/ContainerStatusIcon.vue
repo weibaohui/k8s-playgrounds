9505a2cfe79c9fc35159c97de7d2bd4bc378ed28
weibaohui <weibaohui@yeah.net>
2023-06-27 23:45:54 +0800

    增加容器状态指示

--- src/render/components/ContainerStatusIcon.vue
+++ src/render/components/ContainerStatusIcon.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Circle20Filled } from '@vicons/fluent'
+import { Square16Filled } from '@vicons/fluent'
 
 import { NIcon } from 'naive-ui'
 import { V1Pod } from '../../model/V1Pod'
@@ -11,8 +11,8 @@ const props = defineProps({
 
 <template>
   <span v-for="item in props.pod.status.containerStatuses" :key="item.containerID">
-    <NIcon v-if="item.ready" :size="10" :component="Circle20Filled" color="green" />
-    <NIcon v-else :size="10" :component="Circle20Filled" color="red" />
+    <NIcon v-if="item.ready" :size="10" :component="Square16Filled" color="green" />
+    <NIcon v-else :size="10" :component="Square16Filled" color="red" />
   </span>
 </template>
 
