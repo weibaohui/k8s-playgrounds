7f5049fcabc48ec64be44cd4400206496cfc1027
weibaohui <weibaohui@yeah.net>
2023-07-16 11:45:13 +0800

    exec pty work ok

--- src/main/events/events.gateway.ts
+++ src/main/events/events.gateway.ts
@@ -1,5 +1,4 @@
 import * as process from 'node:process'
-import stream from 'node:stream'
 import os from 'node:os'
 import { IPty } from 'node-pty'
 import { V1Pod } from '@kubernetes/client-node'
@@ -15,7 +14,6 @@ import {
 import { once } from 'lodash'
 import { Observable, from, map } from 'rxjs'
 import { Server } from 'socket.io'
-import { WebSocket } from 'isomorphic-ws'
 
 import * as pty from 'node-pty'
 
@@ -29,8 +27,6 @@ export class EventsGateway {
     private watchService: WatchService,
   ) {}
 
-  private ws: WebSocket = null
-  private ptyPodExec: IPty = null
   ptys = new Map<string, IPty>()
 
   @WebSocketServer()
@@ -50,16 +46,6 @@ export class EventsGateway {
     console.log('terminal receive', data)
     const pk = this.getPty('default', 'forwhile-745849b656-swff6', 'forwhile')
     pk.write(`${data}\r`)
-
-    // ptyProcess.write('ls\r')
-    // ptyProcess.resize(100, 40)
-    // ptyProcess.write('ls\r')
-
-    // const pws = await this.getPodExecWebsocket()
-    // console.log(pws.readyState)
-    // console.log(pws.emit('message111', `0${btoa(data)}`))
-    // console.log(pws.send(`0${btoa(data)}`))
-    // console.log('sended')
   }
 
   private getPty(ns: string, name: string, containerName: string) {
@@ -84,30 +70,6 @@ export class EventsGateway {
     pk.write(`kubectl exec -i -t -n ${ns} ${name} -c ${containerName} -- sh -c "clear; (bash || ash || sh)"\r`)
     this.ptys.set(key, pk)
     return this.ptys.get(key)
-  }
-
-  private async getPodExecWebsocket() {
-    if (this.ws !== null)
-      return this.ws
-
-    const stdout = new stream.PassThrough()
-    stdout.on('data', (r) => {
-      console.log('stdout ondata', r)
-    })
-    const stdin = process.stdin
-    const command = 'bash'
-    this.ws = await this.watchService.execPod('default', 'forwhile-745849b656-swff6', 'forwhile', command, stdout, stdout, stdin, true, (status) => {
-      console.log('execPod status', status)
-    })
-    console.log('open exec pod websocket')
-    // this.ws.on('message111', (d, f) => {
-    //   const data = d.slice(1)
-    //
-    //   console.log('on  message111', atob(data.toString()))
-    //   this.server.emit('terminal', atob(data.toString()))
-    // })
-
-    return this.ws
   }
 
   @SubscribeMessage('watch-init')
