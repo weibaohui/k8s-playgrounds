a6aa22d0f9c3b89a6aafbdb64609b5acd995da47
weibaohui <weibaohui@yeah.net>
2023-07-20 09:21:26 +0800

    补充

--- src/main/watch/watch.model.ts
+++ src/main/watch/watch.model.ts
@@ -4,19 +4,31 @@ export class TerminalData {
   ns: string
   name: string
   containerName: string
-  // 命令
+  /**
+   * 命令
+    */
   command: string
-  // 执行结果
+  /**
+   * 执行结果
+   */
   data: string
-  // resize
+  /**
+   * resize
+   */
   columns: number
   rows: number
+  /**
+   * 日志选项
+   */
   logOptions: {
     showTimestamp: boolean
     follow: boolean
     sinceTimestamp: string
   }
 
+  /**
+   * 心跳
+   */
   lastHeartBeatTime: string
 }
 export class TerminalInstance {
