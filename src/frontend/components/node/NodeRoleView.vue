0a894406d4417b90fdf5a5404401d9395ec2bded
weibaohui <weibaohui@yeah.net>
2023-08-07 23:25:41 +0800

    role add

--- src/frontend/components/node/NodeRoleView.vue
+++ src/frontend/components/node/NodeRoleView.vue
@@ -2,7 +2,7 @@
 import { NTag } from 'naive-ui'
 import { ref } from 'vue'
 import _ from 'lodash'
-import { V1Node } from '../../../model/V1Node'
+import { V1Node } from '../../../backend/k8s/model/V1Node'
 
 const props = defineProps({
   node: V1Node,
@@ -12,6 +12,8 @@ const nodeRole = ref<Array<string>>(new Array<string>())
 function getNodeRole() {
   if (_.has(props.node.metadata.labels, 'node-role.kubernetes.io/control-plane'))
     nodeRole.value.push('control-plane')
+  if (_.has(props.node.metadata.labels, 'node-role.kubernetes.io/agent'))
+    nodeRole.value.push('agent')
 }
 
 getNodeRole()
