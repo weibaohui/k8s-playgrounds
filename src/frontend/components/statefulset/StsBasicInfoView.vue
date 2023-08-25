bb1d0c2f1ab49c58794b8503ebc83e63232b292d
weibaohui <weibaohui@yeah.net>
2023-08-25 21:51:45 +0800

    statefulset update

--- src/frontend/components/statefulset/StsBasicInfoView.vue
+++ src/frontend/components/statefulset/StsBasicInfoView.vue
@@ -14,19 +14,33 @@ const props = defineProps({
     <tbody>
       <tr>
         <td class="left">
+          Replicas
+        </td>
+        <td>
+          {{
+            `${props.sts.status.replicas ? props.sts.status.replicas : 0} desired,
+${props.sts.status.updatedReplicas ? props.sts.status.updatedReplicas : 0} updated,
+${props.sts.status.replicas ? props.sts.status.replicas : 0} total,
+${props.sts.status.availableReplicas ? props.sts.status.availableReplicas : 0} available
+`
+          }}
+        </td>
+      </tr>
+      <tr>
+        <td class="left">
           replicas
         </td>
         <td>{{ props.sts.status.replicas }}</td>
       </tr>
       <tr>
         <td>
-          availableReplicas
+          minReadySeconds
         </td>
-        <td>{{ props.sts.status.availableReplicas ? props.sts.status.availableReplicas : 0 }}</td>
+        <td>{{ props.sts.spec.minReadySeconds }}</td>
       </tr>
       <tr>
-        <td>readyReplicas</td>
-        <td>{{ props.sts.status.readyReplicas ? props.sts.status.readyReplicas : 0 }}</td>
+        <td>updateStrategy</td>
+        <td>{{ props.sts.spec.updateStrategy.type }}</td>
       </tr>
     </tbody>
   </NTable>
