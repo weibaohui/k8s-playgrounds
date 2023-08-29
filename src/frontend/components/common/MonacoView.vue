6bafb60361a82a6ce64cdf94a4cbce9896be3bf1
weibaohui <weibaohui@yeah.net>
2023-08-29 22:28:29 +0800

    monaco update

--- src/frontend/components/common/MonacoView.vue
+++ src/frontend/components/common/MonacoView.vue
@@ -1,42 +1,64 @@
 <script lang="ts" setup>
-import { ref, shallowRef } from 'vue'
-import MonacoEditor from '@liting-yes/vue-monaco-editor'
+import { NButton, NButtonGroup, NHr } from 'naive-ui'
+import { onMounted, ref, toRaw } from 'vue'
+import * as monaco from 'monaco-editor'
 import YAML from 'yaml'
 
 const props = defineProps({
   item: Object,
+  showToolbar: Boolean,
 })
-const options: monaco.editor.IEditorOptions = {
-  language: 'yaml',
-  theme: 'vs-dark',
-  fontSize: 16,
-}
+const emit = defineEmits(['onSaveBtnClicked'])
+const monacoEditorRef = ref()
+const editor = ref<monaco.editor.IStandaloneCodeEditor>()
+onMounted(() => {
+  editor.value = monaco.editor.create(monacoEditorRef.value, {
+    automaticLayout: true,
+    bracketPairColorization: {
+      enabled: true,
+      independentColorPoolPerBracketType: true,
+    },
+    copyWithSyntaxHighlighting: true,
+    cursorSmoothCaretAnimation: 'on',
+    emptySelectionClipboard: false,
+    find: {
+      addExtraSpaceOnTop: true,
+    },
+    foldingImportsByDefault: false,
+    matchOnWordStartOnly: true,
+    mouseWheelZoom: true,
+    smoothScrolling: true,
+    tabSize: 2,
+    wrappingIndent: 'indent',
+    autoClosingBrackets: 'always',
+    autoSurround: 'languageDefined',
+    language: 'yaml',
+    theme: 'vs-dark',
+    fontSize: 16,
+    value: YAML.stringify(props.item),
+  })
+})
 
-const code = ref('')
-const editorRef = shallowRef()
-
-// your action
-function formatCode() {
-  editorRef.value?.getAction('editor.action.formatDocument').run()
+function onSaveBtnClicked() {
+  emit('onSaveBtnClicked', toRaw(editor.value).getValue())
 }
-function getCode() {
-  code.value = YAML.stringify(props.item)
-}
-getCode()
 </script>
 
 <template>
-  <MonacoEditor
-    :ref="editorRef"
-    v-model:value="code"
-    class="editor"
-    :options="options"
-  />
+  <NButtonGroup v-if="props.showToolbar === true">
+    <NButton type="success" @click="onSaveBtnClicked">
+      Save
+    </NButton>
+  </NButtonGroup>
+  <NHr />
+  <div ref="monacoEditorRef" class="vue-monaco-editor" />
 </template>
 
 <style scoped>
-.editor {
+.vue-monaco-editor {
   width: 100%;
   height: 100%;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
 }
 </style>
