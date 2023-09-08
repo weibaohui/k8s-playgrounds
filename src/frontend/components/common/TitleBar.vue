f3df9876bd1e4e3bf00b95bfb72e1e0b4168d67a
weibaohui <weibaohui@yeah.net>
2023-09-08 14:29:47 +0800

    TitleBar

---
+++ src/frontend/components/common/TitleBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { NH6, NText } from 'naive-ui'
+
+const props = defineProps({
+  title: String,
+})
+</script>
+
+<template>
+  <NH6 prefix="bar" align-text type="success">
+    <NText type="success">
+      {{ title }}
+    </NText>
+  </NH6>
+</template>
--- src/frontend/components/ingressClass/IngressClassView.vue
+++ src/frontend/components/ingressClass/IngressClassView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { V1IngressClass } from '@backend/k8s/model/v1IngressClass'
 import InvolvedEventView from '@frontend/components/common/InvolvedEventView.vue'
+import TitleBar from '@frontend/components/common/TitleBar.vue'
 import IngressClassActionView from '@frontend/components/ingressClass/IngressClassActionView.vue'
 import IngressClassBasicInfoView from '@frontend/components/ingressClass/IngressClassBasicInfoView.vue'
-import { NH6, NMessageProvider, NText } from 'naive-ui'
+import { NMessageProvider } from 'naive-ui'
 
 const props = defineProps({
   ingressClass: V1IngressClass,
@@ -14,20 +15,8 @@ const props = defineProps({
   <NMessageProvider>
     <IngressClassActionView :is-dropdown="false" :ingress-class="props.ingressClass" />
   </NMessageProvider>
-  <NH6 prefix="bar" align-text type="success">
-    <NText type="success">
-      Basic Info
-    </NText>
-  </NH6>
+  <TitleBar title="Basic info" />
   <IngressClassBasicInfoView :ingress-class="ingressClass" />
-  <NH6 prefix="bar" align-text type="success">
-    <NText type="success">
-      Events
-    </NText>
-  </NH6>
+  <TitleBar title="Events" />
   <InvolvedEventView :item="props.ingressClass.metadata" />
 </template>
-
-<style scoped>
-
-</style>
