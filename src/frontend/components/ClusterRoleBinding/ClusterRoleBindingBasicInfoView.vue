8893f9fa683309fca77df7195ef121afe805ade3
weibaohui <weibaohui@yeah.net>
2023-09-09 11:36:59 +0800

    ClusterRoleRules & RoleRules View

--- src/frontend/components/ClusterRoleBinding/ClusterRoleBindingBasicInfoView.vue
+++ src/frontend/components/ClusterRoleBinding/ClusterRoleBindingBasicInfoView.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
 import { V1ClusterRoleBinding } from '@backend/k8s/model/v1ClusterRoleBinding'
+import { ResType } from '@backend/k8s/watch/watch.model'
+import ClusterRoleView from '@frontend/components/ClusterRole/ClusterRoleView.vue'
 import ResourceMetadataView from '@frontend/components/common/ResourceMetadataView.vue'
 import TitleBar from '@frontend/components/common/TitleBar.vue'
-import { NTable } from 'naive-ui'
+import { useDrawerService } from '@frontend/service/drawer-service/use-drawer'
+import { K8sService } from '@frontend/service/k8s/K8sService'
+import { DrawerHelper } from '@frontend/service/page/DrawerHelper'
+import { NButton, NTable } from 'naive-ui'
 
 const props = defineProps({
   clusterRoleBinding: V1ClusterRoleBinding,
   showTitle: Boolean,
 
 })
+const drawer = useDrawerService()
+
+async function onClusterRoleNameClick(name: string) {
+  const resource = await K8sService.getResource({
+    resType: ResType.ClusterRole,
+    ns: '',
+    name,
+  })
+  DrawerHelper
+    .instance
+    .drawer(drawer)
+    .show(`${ResType.ClusterRole}:${name}`, ClusterRoleView, { clusterRole: resource })
+}
 </script>
 
 <template>
@@ -19,13 +37,17 @@ const props = defineProps({
     <tbody>
       <tr>
         <td class="left">
-          roleRef
+          ClusterRole
         </td>
-        <td>{{ props.clusterRoleBinding.roleRef }}</td>
+        <td>
+          <NButton type="success" @click="onClusterRoleNameClick(props.clusterRoleBinding.roleRef.name)">
+            {{ props.clusterRoleBinding.roleRef.name }}
+          </NButton>
+        </td>
       </tr>
       <tr>
         <td>
-          rules
+          subjects
         </td>
         <td>{{ props.clusterRoleBinding.subjects }}</td>
       </tr>
