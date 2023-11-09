440c70edccbb7d56e5a47cbe7274daa51728fe60
weibaohui <weibaohui@yeah.net>
2023-11-09 14:32:29 +0800

    lint fix

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,10 +1,9 @@
 declare global {
   interface Window {
     electron: {
-      sendMsg(msg: string): Promise<string>,
+      sendMsg(msg: string): Promise<string>
       onReplyMsg(cb: (msg: string) => any): void
     }
   }
 }
-
 export { }
