703956636e74db7b926cdc4189b96bf61c3fcdd8
weibaohui <weibaohui@yeah.net>
2023-11-13 15:44:05 +0800

    cm keys click

--- src/frontend/components/configmap/CmDataKeysView.vue
+++ src/frontend/components/configmap/CmDataKeysView.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
 import { V1ConfigMap } from '@backend/k8s/model/V1ConfigMap'
+import { ResType } from '@backend/k8s/watch/watch.model'
+import CmView from '@frontend/components/configmap/CmView.vue'
+import { useDrawerService } from '@frontend/service/drawer-service/use-drawer'
+import { ColorHelper } from '@frontend/service/page/ColorHelper'
+import { DrawerHelper } from '@frontend/service/page/DrawerHelper'
+import { NSpace, NTag } from 'naive-ui'
 
 const props = defineProps({
   cm: V1ConfigMap,
 })
+const drawer = useDrawerService()
+
+function show() {
+  DrawerHelper
+    .instance
+    .drawer(drawer)
+    .show(`${ResType.ConfigMap}:${props.cm.metadata.name}`, CmView, { cm: props.cm })
+}
 </script>
 
 <template>
-  <NTag v-for="(v, k) in props.cm.data" :key="k">
-    {{ k }}
-  </NTag>
+  <NSpace v-for="(v, k) in props.cm.data" :key="k" :vertical="false">
+    <NTag :color="{ color: ColorHelper.randomColor() }">
+      <span @click="show">
+        {{ k }}
+      </span>
+    </NTag>
+  </NSpace>
 </template>
 
 <style scoped>
